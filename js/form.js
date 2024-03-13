23cb9d5a5a735432b3b581a79d9dd1ac64cbe184
Anselmo Alva <memin12882@gmail.com>
2024-03-13 15:43:54 -0600

    Whatsapp

--- js/form.js
+++ js/form.js
@@ -4,7 +4,7 @@ document.querySelector("#submit").addEventListener("click", e => {
   //INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
   const telefono = "522223592373";
 
-  const nombre = document.querySelector("#cliente").value;
+  const nombre = document.querySelector("#nombre").value;
   const fecha = document.querySelector("#fecha").value;
   const hora = document.querySelector("#hora").value;
   const consulta = document.querySelector("#consulta").value;
@@ -17,16 +17,16 @@ document.querySelector("#submit").addEventListener("click", e => {
   const url = `https://api.whatsapp.com/send?phone=${telefono}&text=
 		*_Dra. Patricia Geriatra_*%0A
 		*Cita*%0A%0A
-		*Nombre:*%0A
+		*Nombre*%0A
 		${nombre}%0A
-		*Indica la fecha de tu cita:*%0A
+		*Indica la fecha de tu cita*%0A
 		${fecha}%0A
-		*Indica la hora de tu cita:*%0A
+		*Indica la hora de tu cita*%0A
 		${hora}%0A
-		*Motivo de consulta:*%0A
+		*Motivo de consulta*%0A
 		${consulta}`;
 
-  if (nombre === "" || fecha === "" || hora === "") {
+  if (nombre === "" || fecha === "" || hora === "" | consulta === "") {
     resp.classList.add("fail");
     resp.innerHTML = `Faltan algunos datos, ${nombre}`;
     return false;
